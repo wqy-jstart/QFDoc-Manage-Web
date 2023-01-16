4105d12604ca44a9b1c77ed10bfda19da64c9158
Devotion <2168149199@qq.com>
2023-01-16 20:29:10 +0800

    ....

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,8 +1,35 @@
+<style>
+body {
+  background-image: url("../../public/background.png");
+  background-size: cover; /*设置封面*/
+}
+
+.layout-header {
+  background: rgba(255, 255, 255, 0.5) !important;
+  color: #fff;
+  text-align: left;
+  line-height: 60px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 25px;
+}
+</style>
+
 <template>
   <div>
-    <h1 style="text-align: center">
-      欢迎来到主页
-    </h1>
+    <el-container>
+    <el-header class="layout-header">
+      <div class="block">
+        <h1 style="color: black;font-size: 40px;font-family: 方正姚体">清风文档管理系统
+        </h1>
+      </div>
+    </el-header>
+      <el-main>
+
+      </el-main>
+    </el-container>
   </div>
 </template>
 

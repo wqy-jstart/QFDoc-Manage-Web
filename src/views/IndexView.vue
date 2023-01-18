d3eae165465ee9ec3a8871c487f259b025a1a805
Devotion <2168149199@qq.com>
2023-01-18 20:50:25 +0800

    ....

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -10,8 +10,9 @@ body {
   text-align: left;
   line-height: 60px;
 }
-.layout-main{
-  text-align: center;
+
+.layout-main {
+  text-align: center !important;
 }
 
 a {
@@ -30,23 +31,27 @@ a {
         </div>
       </el-header>
       <el-main class="layout-main">
-        <el-form>
-          <p style="font-size: 30px;font-family: 方正姚体">请选择要上传的图片:</p>
-          <el-form-item prop="url" style="margin: 10px">
-            <el-upload
-                action="http://localhost:9801/upload/image"
-                name="file"
-                list-type="picture-card"
-                :on-success="handleSuccess"
-                :on-preview="handlePictureCardPreview"
-                :on-remove="handleRemove">
-              <i class="el-icon-plus"></i>
-            </el-upload>
-            <el-dialog :visible.sync="dialogVisible">
-              <img width="100%" :src="dialogImageUrl" alt="">
-            </el-dialog>
-          </el-form-item>
-        </el-form>
+        <!--创建空间名的表单-->
+        <span style="font-size: 20px;font-family: 方正姚体">请输入空间名称:</span>
+        <el-input style="width: 300px" type="text" v-model="bucketName" placeholder="请输入空间名：" maxlength="10"
+                  show-word-limit></el-input>
+        <el-button style="margin-left: 10px" type="primary" @click="submitForm()">创建
+        </el-button>
+
+        <!--上传图片的表单-->
+        <p style="font-size: 20px;font-family: 方正姚体">请选择要上传的图片:</p>
+        <el-upload
+            class="upload-demo"
+            ref="upload"
+            name="file"
+            action="http://localhost:9801/upload/image"
+            :on-success="handleSuccess"
+            :on-remove="handleRemove"
+            :auto-upload="false">
+          <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+          <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
       </el-main>
     </el-container>
   </div>
@@ -56,33 +61,38 @@ a {
 export default {
   data() {
     return {
+      image: '',
+      bucketName: '',
       dialogImageUrl: '', // 绑定图片路径
       dialogVisible: false,
-      url:'',
     }
   },
   methods: {
+    submitUpload() {
+      this.$refs.upload.submit();
+    },
     handleRemove(file, fileList) {
       console.log(file, fileList);
-      let url = this.GLOBAL.systemUrl+'upload/remove?url=' + file.response.data;
+      if (file.response == null){
+        this.$message.warning("文件暂未上传，删除失败！")
+      }
+      let url = this.GLOBAL.systemUrl + 'upload/remove?url=' + file.response.data;
       console.log("发送删除图片的请求")
       this.axios
           .create({
             'headers': {
               'Authorization': localStorage.getItem('jwt')
             }
-          }).get(url).then((response)=>{
+          }).get(url).then((response) => {
         console.log("删除服务器图片完成!")
       })
     },
-    handlePictureCardPreview(file) {
-      this.dialogImageUrl = file.url;
-      this.dialogVisible = true;
-    },
     handleSuccess(response, file, fileList) {//调用后可直接接收响应过来的数据
-      this.url = response;//将响应的"唯一文件名图片路径"添加到weibo对象中
       console.log(response)
     },
+    submitForm() {
+
+    }
   }
 }
 </script>

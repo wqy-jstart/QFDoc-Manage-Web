8f363f1a5309e82c5e4144694623990074f824cd
Devotion <2168149199@qq.com>
2023-01-26 21:03:16 +0800

    权限控制！

--- src/views/sys-admin/main/user/AssignPermission.vue
+++ src/views/sys-admin/main/user/AssignPermission.vue
@@ -2,7 +2,7 @@
   <div>
     <el-breadcrumb separator-class="el-icon-arrow-right" style="font-size: 16px">
       <el-breadcrumb-item :to="{ path: '/auth' }">权限管理</el-breadcrumb-item>
-      <el-breadcrumb-item>分配权限</el-breadcrumb-item>
+      <el-breadcrumb-item>分配角色</el-breadcrumb-item>
     </el-breadcrumb>
 
     <el-divider></el-divider>
@@ -17,51 +17,18 @@
       <el-table-column prop="gmtModified" label="修改时间" align="center"></el-table-column>
       <el-table-column label="操作" align="center">
         <template slot-scope="scope">
-          <el-button type="primary" icon="el-icon-edit" size="small"
-                     @click="handleEdit(scope.row)">修改
-          </el-button>
           <el-button type="warning" icon="el-icon-s-help" size="small"
-                     @click="setPassport(scope.row)">分配权限</el-button>
+                     @click="setPassport(scope.row)">分配角色
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <!-- 弹出的编辑相册的对话框 -->
-    <el-dialog width="40%" title="修改角色信息:" :visible.sync="dialogFormVisibleToPermission">
-      <el-form :model="ruleForm">
-        <el-form-item label="用户名:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码:" :label-width="formLabelWidth">
-          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="昵称:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.nickname" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="头像url:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.avatar" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="性别:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.gender" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="年龄:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.age" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号:" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.phone" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisibleToPermission = false">取 消</el-button>
-        <el-button type="primary" @click="submitEditToPermission()">确 定</el-button>
-      </div>
-    </el-dialog>
-
-    <!-- 弹出的编辑相册的对话框 -->
-    <el-dialog width="40%" title="分配权限" :visible.sync="dialogFormVisibleToAssign">
+    <el-dialog width="40%" title="分配权限:" :visible.sync="dialogFormVisible">
       <el-form :model="ruleForm">
         <el-form-item label="角色名" :label-width="formLabelWidth">
-          <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
+          <el-input v-model="ruleForm.name" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="权限列表" :label-width="formLabelWidth">
           <template>
@@ -70,14 +37,14 @@
             <div style="margin: 15px 0;"></div>
             <!--label标签对应选择的多选框Id-->
             <el-checkbox-group v-model="checkedRoles" @change="handleCheckedCitiesChange">
-              <el-checkbox v-for="role in roles" :label="role.id">{{ role.name }}</el-checkbox>
+              <el-checkbox v-for="permission in permissions" :label="permission.id">{{ permission.name }}</el-checkbox>
             </el-checkbox-group>
           </template>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisibleToAssign = false">取 消</el-button>
-        <el-button type="primary" @click="submitEditToAssign()">保存</el-button>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitEdit()">保存</el-button>
       </div>
     </el-dialog>
   </div>
@@ -89,25 +56,26 @@ export default {
     return {
       tableData: [],
       ruleForm: {
-        username: '',
-        role: ''
+        name: '',
       },
-      roleIds:[], // 传递选中的角色id
-      oldRoleIds:[],
-      dialogFormVisibleToPermission:false,
-      dialogFormVisibleToAssign: false,
+      permissionIds: [], // 传递选中的角色id
+      oldPermissionIds: [],
+      dialogFormVisible: false,
       formLabelWidth: '120px',
       checkAll: false,
       checkedRoles: [], // 勾选过的角色
-      roles: [],// 显示全部角色
+      permissions: [],// 显示全部角色
       isIndeterminate: true
     }
   },
   methods: {
-    // 保存修改
-    submitEditToPermission() {
-      let url = this.GLOBAL.systemUrl + "users/update";
-      let formString = this.qs.stringify(this.ruleForm)
+    // 保存
+    submitEdit() {
+      // 将选择的角色传递给后端插入到用户角色关联表中
+      let object = {name: this.ruleForm.name, permissionIds: this.checkedRoles, oldPermissionIds: this.oldPermissionIds};
+      console.log("角色：" + this.checkedRoles)
+      let url = this.GLOBAL.systemUrl + "permissions/assignToPermission";
+      let formString = this.qs.stringify(object);
       this.axios
           .create({
             'headers': {
@@ -116,64 +84,20 @@ export default {
           }).post(url, formString).then((response) => {
         let responseBody = response.data;
         if (responseBody.state == 20000) {
-          this.$message.success("修改成功！")
-          this.dialogFormVisibleToPermission = false;
-          this.loadAdminList();
-        } else {
-          this.$message.error(responseBody.message);
-        }
-      })
-    },
-    // 编辑修改按钮
-    handleEdit(role) {
-      let url = this.GLOBAL.systemUrl + 'roles/' + role.id + '/selectById';
-      console.log(url);
-      this.axios
-          .create(
-              {
-                'headers': {'Authorization': localStorage.getItem('jwt')}
-              }
-          )
-          .post(url).then((response) => {
-        let responseBody = response.data;
-        if (responseBody.state == 20000) {
-          this.ruleForm = responseBody.data;
-          this.dialogFormVisibleToPermission = true;
-        } else {
-          this.$message.error(responseBody.message);
-          this.loadAdminList();
-        }
-      })
-    },
-    // 保存权限
-    submitEditToAssign() {
-      // 将选择的角色传递给后端插入到用户角色关联表中
-      let object = {username:this.ruleForm.username,roleIds:this.checkedRoles,oldRoleIds:this.oldRoleIds};
-      console.log("角色："+this.checkedRoles)
-      let url = this.GLOBAL.systemUrl + "userRoles/assignToRole";
-      let formString = this.qs.stringify(object);
-      this.axios
-          .create({
-            'headers': {
-              'Authorization': localStorage.getItem('jwt')
-            }
-          }).post(url,formString).then((response)=>{
-        let responseBody = response.data;
-        if (responseBody.state == 20000){
           this.$message.success("分配成功！")
-          this.oldRoleIds = [];
-          this.dialogFormVisibleToAssign = false;
-        }else {
+          this.oldPermissionIds = [];
+          this.dialogFormVisible = false;
+        } else {
           this.$message.error(responseBody.message);
         }
       })
     },
-    // 点击弹框分配权限
+    // 点击弹框
     setPassport(admin) {
-      this.dialogFormVisibleToAssign = true;
-      this.ruleForm.username = admin.name;
+      this.dialogFormVisible = true;
+      this.ruleForm.name = admin.name;
       // 查询所有角色，赋值给cities
-      let url = this.GLOBAL.systemUrl + "roles" // 请求路径
+      let url = this.GLOBAL.systemUrl + "permissions" // 请求路径
       console.log('url=' + url);
       this.axios
           .create({
@@ -184,13 +108,13 @@ export default {
           .get(url).then((response) => {// 发送异步请求
         let responseBody = response.data;
         if (responseBody.state == 20000) {
-          this.roles = responseBody.data;//将获取响应的数据中的data数据赋值给tableData
+          this.permissions = responseBody.data;//将获取响应的数据中的data数据赋值给tableData
         } else {
           this.$message.error(responseBody.message);
         }
       })
       // 查询当前角色
-      let url1 = this.GLOBAL.systemUrl + 'roles/' + admin.id + '/selectToUserId';
+      let url1 = this.GLOBAL.systemUrl + 'permissions/' + admin.id + '/selectToRoleId';
       this.axios
           .create({
             'headers': {
@@ -201,8 +125,7 @@ export default {
         let responseBody = response.data;
         if (responseBody.state == 20000) {
           this.checkedRoles = responseBody.data;//将获取响应的数据中的data数据赋值给tableData
-          this.oldRoleIds = responseBody.data; //记录原来分配的角色
-          console.log("原来："+this.oldRoleIds)
+          this.oldPermissionIds = responseBody.data; //记录原来分配的角色
         } else {
           this.$message.error(responseBody.message);
         }
@@ -211,17 +134,17 @@ export default {
     // 全选的事件
     handleCheckAllChange(val) {
       console.log("点击全选")
-      this.checkedRoles = val ? this.roles : [];
+      this.checkedRoles = val ? this.permissions : [];
       this.isIndeterminate = false;
-      let url = this.GLOBAL.systemUrl + "roles/selectRoleIds";
+      let url = this.GLOBAL.systemUrl + "permissions/selectPermissionIds";
       this.axios
           .create({
             'headers': {
               'Authorization': localStorage.getItem('jwt')
             }
-          }).get(url).then((response)=>{
+          }).get(url).then((response) => {
         let responseBody = response.data;
-        if (responseBody.state == 20000){
+        if (responseBody.state == 20000) {
           this.checkedRoles = responseBody.data;
         }
       })
@@ -231,8 +154,8 @@ export default {
       console.log("点击勾选")
       console.log(value)
       let checkedCount = value.length;
-      this.checkAll = checkedCount === this.roles.length;
-      this.isIndeterminate = checkedCount > 0 && checkedCount < this.roles.length;
+      this.checkAll = checkedCount === this.permissions.length;
+      this.isIndeterminate = checkedCount > 0 && checkedCount < this.permissions.length;
     },
     // 该方法用来请求相册的列表数据
     loadAdminList() {
@@ -253,7 +176,6 @@ export default {
   },
   // 生命周期方法(挂载)
   mounted() {
-    console.log('mounted');
     this.loadAdminList();
   }
 }
